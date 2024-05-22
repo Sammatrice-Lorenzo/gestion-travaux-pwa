c6b550659eaad5c079ae79f30647e8ec7bf522ef
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2024-05-23 00:03:14 +0200

    update dockerfile adn nginx.conf

--- src/js/messages.js
+++ src/js/messages.js
@@ -1,7 +1,7 @@
 export const SUCCESS_INSERTION_FORM = 'L\'insertion a été bien prise en compte!'
 export const SUCCESS_UPDATE_FORM = 'La modification a été bien prise en compte!'
 export const SUCCESS_DELETE_FORM = 'La suppression a été bien prise en compte!'
-export const ERROR_SERVER = 'La requête n\'as pa pu aboutir'
+export const ERROR_SERVER = 'La requête n\'as pas pu aboutir'
 export const CONFIRMATION_TO_DELETE = 'Vous êtes sûr de vouloir supprimer ?'
 export const TOKEN_EXPIRED = 'Le token a expiré veuillez vous reconnecter !'
 
